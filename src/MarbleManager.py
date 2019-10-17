64d3d26d763cc42a296aa61264e82d0084146b44
wargen <wargenng@gmail.com>
2019-10-17 12:25:51 -0700

    fixed database issue with brute force

--- src/MarbleManager.py
+++ src/MarbleManager.py
@@ -2,43 +2,42 @@ import random
 import shelve
 
 class MarbleManager:
-
-    def __init__(self):
-        self.marbleRecords = shelve.open("data", writeback=True)
-    
     async def register(self, author, channel):
         server = str(channel.guild.id) 
-        if server not in self.marbleRecords: #if server doesn't have record
-            self.marbleRecords[server]={} #create new empty dict
+        marbleRecords = shelve.open('data', writeback=True)
+        if server not in marbleRecords: #if server doesn't have record
+            marbleRecords[server]={} #create new empty dict
             
-        if author.id in self.marbleRecords[server]: 
+        if author.id in marbleRecords[server]: 
             await channel.send("You are already registered dummy.")
         else: 
             randomAmount = random.randrange(20,41,1) #20 to 40 inclusive
-            self.marbleRecords[server][author.id] = randomAmount
+            marbleRecords[server][author.id] = randomAmount
             name = author.name if author.nick == None else author.nick
             await channel.send( str(name) + " has registered and recieved " + str(randomAmount) + " marbles" + ("!" if randomAmount>=38 else ".") )
             
     async def getCollection(self, author, channel):
         server = str(channel.guild.id)
-        if server not in self.marbleRecords or author.id not in self.marbleRecords[server]:
+        marbleRecords = shelve.open('data', writeback=True)
+        if server not in marbleRecords or author.id not in marbleRecords[server]:
             await channel.send("You are not registered dummy.") 
         else:
-            userAmount = self.marbleRecords[server][author.id]
+            userAmount = marbleRecords[server][author.id]
             name = author.name if author.nick == None else author.nick
             await channel.send( str(name) + ": " + str(userAmount) + (" marble." if userAmount==1 else " marbles.") )     
             
     async def coinflip(self, author, channel, bet):
         server = str(channel.guild.id)
+        marbleRecords = shelve.open('data', writeback=True)
         name = author.name if author.nick == None else author.nick
-        if self.marbleRecords[server][author.id] >= bet:
+        if marbleRecords[server][author.id] >= bet:
             outcome = random.choice([0,1])
             if outcome == 1:
                 await channel.send( str(name) + " won " + str(bet) + (" marble." if bet==1 else " marbles.") )
-                self.marbleRecords[server][author.id] += bet
+                marbleRecords[server][author.id] += bet
             else:
                 await channel.send( str(name) + " lost " + str(bet) + (" marble." if bet==1 else " marbles.") )
-                self.marbleRecords[server][author.id] -= bet
+                marbleRecords[server][author.id] -= bet
         else:
             await channel.send("why you trying to break my bot")
 marbleManager = MarbleManager()
